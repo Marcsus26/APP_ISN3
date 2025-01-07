727d8f8b4233ab02f1849cf4376cc80c90a506db
marcsus26 <marc.doumit@insa-lyon.fr>
2025-01-07 15:48:56 +0100

    fixed dijkstra

--- calcul.py
+++ calcul.py
@@ -69,8 +69,6 @@ def dijkstra(graph,start,goal):
     for node in unseenNodes:
         shortest_distance[node] = infinity
     shortest_distance[start] = 0
-
-    # Determine which is minimum node. What does that mean?
     while unseenNodes:
         minNode = None
         for node in unseenNodes:
@@ -79,21 +77,20 @@ def dijkstra(graph,start,goal):
             elif shortest_distance[node] < shortest_distance[minNode]:
                 minNode = node
 
-        for edge, weight in graph[minNode].items():
-            if weight + shortest_distance[minNode] < shortest_distance[edge]:
-                shortest_distance[edge] = weight + shortest_distance[minNode]
-                predecessor[edge] = minNode
+        for childNode, weight in graph[minNode].items():
+            if weight + shortest_distance[minNode] < shortest_distance[childNode]:
+                shortest_distance[childNode] = weight + shortest_distance[minNode]
+                predecessor[childNode] = minNode
         unseenNodes.pop(minNode)
 
     currentNode = goal
     while currentNode != start:
         try:
-            path.insert(0,currentNode)
+            path.insert(0, currentNode)
             currentNode = predecessor[currentNode]
         except KeyError:
             print('Path not reachable')
             break
-    path.insert(0,start)
+    path.insert(0, start)
     if shortest_distance[goal] != infinity:
-        print('Shortest distance is ' + str(shortest_distance[goal]))
-        print('And the path is ' + str(path))
+        return path
